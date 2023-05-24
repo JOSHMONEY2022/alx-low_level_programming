a765bd199d53e3fe2a31f4691916314c4b9a503a
JOSHMONEY2022 <echudejoshua@gmail.com>
2023-05-24 12:03:38 -0700

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,32 @@
-bef1 = (bef / l);
-bef2 = (bef % l);
-aft1 = (aft / l);
-aft2 = (aft % l);
+#include <stdio.h>
+#include "main.h"
 
-	for (i = 92; i < 99; ++i)
+/**
+* print_to_98 - prints all natural numbers from n to 98,
+* followed by a new line
+* @n: print from this number
+*/
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			printf("%d, ", i);
+			else if (i == 98)
+			printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+			printf("%d\n", j);
+		}
 	}
-	printf("\n");
-	return (0);
 }
